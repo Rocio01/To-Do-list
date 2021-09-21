cb242178c2580f1acd0b8505c6fcd1de44cc87a1
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-21 16:48:03 -0500

    Add class ToDo, with add delete and change methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-import './style.css';
+// import './style.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const toDoArray = [];
+
+const generateID = () => Math.floor(Math.random() * 10000);
+class ToDo {
+  constructor(description, completed) {
+    this.description = description;
+    this.completed = completed;
+    this.id = generateID();
+  }
+
+  addTodo() {
+    toDoArray.push(this);
+  }
+
+  deleteToDo() {
+    toDoArray.splice(toDoArray.indexOf(this.id), 1);
+    return toDoArray;
+  }
+
+  changeStatus() {
+    this.completed = !this.completed;
+  }
+}
+
