bce97119ac4909966b40d7c8fff8e84ecb616aac
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-21 15:04:10 -0500

    Add index.html template

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 import './style.css';
-alert("hello world");
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
+      template: './src/index.html'
     }),
   ],
 
