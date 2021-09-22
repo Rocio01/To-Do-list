cedbabdc2a2dbf90a1384e0bbfc0de24d3e5e15f
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-21 20:55:10 -0500

    Add display functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
-// import './style.css';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const toDoArray = [];
-
-
 
 class ToDo {
   constructor(description, completed = false) {
     this.description = description;
     this.completed = completed;
-    
   }
 
   addTodo() {
@@ -27,3 +24,26 @@ class ToDo {
   }
 }
 
+const firstTask = new ToDo('do breakfast', true);
+firstTask.addTodo();
+const secondTask = new ToDo('morning session');
+secondTask.addTodo();
+const thirdTask = new ToDo('lunch');
+thirdTask.addTodo();
+
+const listContainer = document.querySelector('.list-container');
+
+const displayTodos = () => {
+  toDoArray.forEach((todo) => {
+    const li = document.createElement('li');
+
+    li.innerHTML = `<li class="list-group-item">
+                  <input class="form-check-input me-1" type="checkbox" value="" aria-label="...">
+                  ${todo.description}
+                  </li>`;
+
+    listContainer.appendChild(li);
+  });
+};
+
+displayTodos();
