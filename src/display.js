5d85bf5d6d19a6d557d047d514d6776a34dd815b
Zulma Rocio <zrmartinezg@gmail.com>
2021-09-23 10:28:45 -0500

    Add text-decoration-line-through

--- src/display.js
+++ src/display.js
@@ -4,13 +4,13 @@ const displayTodos = (arr) => {
   arr.forEach((todo) => {
     const li = document.createElement('li');
     if (todo.completed === true) {
-      li.innerHTML = `<li class="list-group-item">
-      <input class="form-check-input me-1 check" id="${todo.index}" type="checkbox"  checked value="" aria-label="...">
-      ${todo.description}
+      li.innerHTML = `<li class="list-group-item text-decoration-line-through">
+      <input class="form-check-input me-1 check " id="${todo.index}" type="checkbox"  checked value="" onClick="window.location.reload()" >
+        ${todo.description}
       </li>`;
     } else {
       li.innerHTML = `<li class="list-group-item">
-      <input class="form-check-input me-1 check" id="${todo.index}" type="checkbox"  value="" aria-label="...">
+      <input class="form-check-input me-1 check" id="${todo.index}" type="checkbox"  value="" onClick="window.location.reload()">
       ${todo.description}
       </li>`;
     }
