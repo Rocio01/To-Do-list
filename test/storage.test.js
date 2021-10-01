c23cb25c38c858516684528a7ae6d5f0a7f179f7
AlexHamn <agalexandro16@gmail.com>
2021-10-01 15:44:01 -0500

    Fix eslint errors

--- test/storage.test.js
+++ test/storage.test.js
@@ -1,60 +1,60 @@
 import {
-    addTodoLs,
-    getTodosls,
-    deleteTodoLS,
+  addTodoLs,
+  getTodosls,
+  deleteTodoLS,
 } from '../src/storage';
 
 class LocalStorageMock {
-    constructor() {
-        this.store = {};
-    }
+  constructor() {
+    this.store = {};
+  }
 
-    clear() {
-        this.store = {};
-    }
+  clear() {
+    this.store = {};
+  }
 
-    getItem(key) {
-        return this.store[key] || null;
-    }
+  getItem(key) {
+    return this.store[key] || null;
+  }
 
-    setItem(key, value) {
-        this.store[key] = String(value);
-    }
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
 
-    removeItem(key) {
-        delete this.store[key];
-    }
+  removeItem(key) {
+    delete this.store[key];
+  }
 }
 
 global.localStorage = new LocalStorageMock();
 
 describe('addTodoLs', () => {
-    const first = { description: 'test' };
-    addTodoLs(first);
-    const todos = getTodosls();
-    const wrong = { description: "I'm not in lS" };
+  const first = { description: 'test' };
+  addTodoLs(first);
+  const todos = getTodosls();
+  const wrong = { description: "I'm not in lS" };
 
-    test('Add todo to local storage ', () => {
-        expect(todos).toEqual([{ description: 'test', index: 0 }]);
-    });
+  test('Add todo to local storage ', () => {
+    expect(todos).toEqual([{ description: 'test', index: 0 }]);
+  });
 
-    test('getting the right object', () => {
-        expect(todos).not.toEqual([wrong]);
-    });
+  test('getting the right object', () => {
+    expect(todos).not.toEqual([wrong]);
+  });
 });
 
 describe('deleteTodoLS', () => {
-    const second = { description: 'It deletes an item from the array' };
-    addTodoLs(second);
+  const second = { description: 'It deletes an item from the array' };
+  addTodoLs(second);
 
-    deleteTodoLS({ description: 'test', index: 0 });
-    const todos = getTodosls();
+  deleteTodoLS({ description: 'test', index: 0 });
+  const todos = getTodosls();
 
-    test('Delete todo from localStorage ', () => {
-        expect(todos).toEqual([{ description: 'It deletes an item from the array', index: 1 }]);
-    });
+  test('Delete todo from localStorage ', () => {
+    expect(todos).toEqual([{ description: 'It deletes an item from the array', index: 1 }]);
+  });
 
-    test('Retrieves the correct element after delete ', () => {
-        expect(todos).not.toEqual([{ description: 'test', index: 0 }, { description: 'It deletes an item from the array', index: 1 }]);
-    });
+  test('Retrieves the correct element after delete ', () => {
+    expect(todos).not.toEqual([{ description: 'test', index: 0 }, { description: 'It deletes an item from the array', index: 1 }]);
+  });
 });
